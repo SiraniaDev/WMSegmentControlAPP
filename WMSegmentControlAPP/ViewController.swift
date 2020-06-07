d442ba05ed01daa38e8596b09449da64e5fd7d04
Nadia M <siraniadev@gmail.com>
2020-06-07 07:21:35 +0200

    Initial Commit

---
+++ WMSegmentControlAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WMSegmentControlAPP
+//
+//  Created by Nadia Mettioui on 07/06/2020.
+//  Copyright © 2020 Nadia Mettioui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
